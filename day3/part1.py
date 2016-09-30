19c3a85db3b199ccd16cf426338c77e1b44750f8
Joe Arthur <joearthur@jqb-joe.local>
2016-09-30 16:38:03 +0100

    Added day3

---
+++ day3/part1.py
@@ -0,0 +1,23 @@
+positions = [(0, 0), ]
+pos = {'lat': 0, 'lon': 0}
+
+with open('input.txt', 'r') as input_txt:
+    while True:
+        char = input_txt.read(1)
+        if not char:
+            break
+        if char == '^':
+            pos['lat'] -= 1
+        elif char == '>':
+            pos['lon'] += 1
+        elif char == 'v':
+            pos['lat'] += 1
+        elif char == '<':
+            pos['lon'] -= 1
+
+        positions.append((pos['lat'], pos['lon']))
+
+unique = len(set(positions))
+
+print unique
+
