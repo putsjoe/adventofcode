8fbdb7514ab1d4f7a17e59ae1604d881c0a84060
Joe Arthur <joe@bullionbypost.co.uk>
2017-12-13 09:19:40 +0000

    Formatting

--- 2017/day1/part2.py
+++ 2017/day1/part2.py
@@ -1,19 +1,14 @@
 
-
 f = open('input.txt', 'r')
-line = f.readline().strip()
-
-line = list(line) 
+line = list(f.readline().strip())
 
 total = 0
 
-for pos, number in enumerate(line):
-    previous_number = line[pos-int(len(line)/2)]
+for position, number in enumerate(line):
+    previous_number = line[position-int(len(line)/2)]
 
     if number == previous_number:
         total += int(number)
 
 print total
-
-
 
